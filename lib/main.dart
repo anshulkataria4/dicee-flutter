f83709f20dc1ab65fe574e05808bfd0fd9422e0d
Anshul Kataria <flunkeyit@gmail.com>
2019-12-29 01:23:01 +0530

    stateful widget and random function added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,27 +16,37 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
-  int leftDiceNumber=1;
-  int rightDiceNumber=4;
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    leftDiceNumber=2;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              onPressed: (){
-                print('Left Buttton Pressed');
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6)+1;
+                });
+                print('Left Buttton Pressed$leftDiceNumber');
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
-              onPressed: (){
-                print('Right Buttton Pressed');
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6)+1;
+                });
+                print('Right Buttton Pressed$rightDiceNumber');
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
