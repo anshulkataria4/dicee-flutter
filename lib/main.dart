a4190cc36272a72d47c442744305fb2918c31d00
Anshul Kataria <flunkeyit@gmail.com>
2019-12-29 01:07:49 +0530

    adding variables

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,28 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  int leftDiceNumber=1;
+  int rightDiceNumber=4;
   @override
   Widget build(BuildContext context) {
+    leftDiceNumber=2;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: (){
+                print('Left Buttton Pressed');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/dice2.png'),
+            child: FlatButton(
+              onPressed: (){
+                print('Right Buttton Pressed');
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
